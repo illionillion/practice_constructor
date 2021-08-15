572d091dcfa1b8473daf1bd18ff3e0c6e1d44313
illionillion <yuto.0526.sano@gmail.com>
2021-08-16 01:27:29 +0900

    first commit

---
+++ main.js
@@ -0,0 +1,49 @@
+function main(obj){
+  console.log(this);
+  this.speed=obj;
+  this.sum=obj;
+  // this.animation=function(){
+  //   document.querySelector('#main').animate(
+  //     [
+  //       { opacity: 0 }, // 最初の状態（表示）
+  //       { backgroundColor: 'blue' }, // 最初の状態（表示）
+  //       { backgroundColor: 'red' }, // 最初の状態（表示）
+  //       { opacity: 1 } // 最後の状態 (非表示)
+  //     ],
+  //     this.speed//の速さで実行
+  //   )
+  //   // this.sum=this.speed;
+  // };
+}
+
+main.prototype.animation=function(){//prototypeの方がいい？
+
+  document.querySelector('#main').animate(
+    [
+      { opacity: 0 }, // 最初の状態（表示）
+      { backgroundColor: 'blue' }, // 最初の状態（表示）
+      { backgroundColor: 'red' }, // 最初の状態（表示）
+      { opacity: 1 } // 最後の状態 (非表示)
+    ],
+    this.speed//の速さで実行
+  )
+  // this.sum=this.speed;
+
+};
+
+let m;
+    m=new main(0);//コンストラクタ？
+window.onload=function(){
+
+  document.querySelector('#btn').addEventListener('click',()=>{
+    let time=document.querySelector('#time_num').value;
+    // let m=new main(time);//コンストラクタ？
+    m.speed=time;
+    m.animation();
+    m.sum=Number(m.sum)+Number(m.speed);
+
+    console.log(m.speed);
+  
+  })
+  
+}
